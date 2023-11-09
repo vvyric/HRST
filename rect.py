f90dd08297b4b1ebcea51829bd6b6c062a003dfb
vvyric <131678505+vvyric@users.noreply.github.com>
2023-11-09 10:44:11 +0100

    Update rect.py

--- rect.py
+++ rect.py
@@ -1,3 +1,29 @@
+import cv2 
+import numpy as np 
+  
+  
+# Read image 
+image = cv2.imread("image.png") 
+  
+# Select ROI 
+r = cv2.selectROI("select the area", image) 
+  
+# Crop image 
+cropped_image = image[int(r[1]):int(r[1]+r[3]),  
+                      int(r[0]):int(r[0]+r[2])] 
+  
+# Display cropped image 
+cv2.imshow("Cropped image", cropped_image) 
+cv2.waitKey(0)
+
+
+
+
+
+
+
+
+
 import cv2
 import numpy as np
 
